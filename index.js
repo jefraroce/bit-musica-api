5f3da734bd16fcaf1606d03c1b8bead48445b38f
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-05-13 19:37:42 -0500

    Primera interacción con MongoDB

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const PUERTO = 3000
+
+app.get('/', function(solicitud, respuesta) {
+  console.log('solicitud.query ', solicitud.query)
+  respuesta.send('Hola Mundo')
+})
+
+// mongodb+srv://DB_USER:DB_PASSWORD@DB_HOST:DB_PORT/DB_NAME
+mongoose.connect('mongodb://127.0.0.1:27017/bit_musica', {useNewUrlParser: true})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Error de conexión: '))
+db.once('open', function() {
+  console.log('La conexión a la DB fue exitosa!')
+
+  const cancionSchema = new mongoose.Schema({
+    nombre: String,
+    artista: String,
+    album: String,
+    enlace: String
+  })
+
+  const Cancion = mongoose.model('canciones', cancionSchema)
+
+  Cancion.find(function(error, canciones) {
+    if (error) {
+      console.error('Error consultando canciones: ', error)
+    } else {
+      console.log('canciones ', canciones)
+    }
+  })
+})
+
+app.listen(PUERTO)
