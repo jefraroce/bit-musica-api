0d35a52d9becbefab19675014afb6101a782a07d
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-05-14 20:18:42 -0500

    Agregada ruta para actualizar canciones.

--- componentes/canciones/rutas.js
+++ componentes/canciones/rutas.js
@@ -2,35 +2,69 @@ const express = require('express')
 const router = express.Router()
 const Cancion = require('./modelo')
 
-router.get('/', function(solicitud, respuesta) {
-  Cancion.find( function(error, canciones) {
+/**
+ * Consulta de todas las canciones
+ */
+router.get('/', function (solicitud, respuesta) {
+  Cancion.find(function (error, canciones) {
     if (error) {
       console.error('Error consultando canciones: ', error)
+      respuesta.send('Error consultando las canciones.')
     } else {
       respuesta.send({ canciones: canciones })
     }
   })
 })
 
-router.get('/:id', function(solicitud, respuesta) {
-  Cancion.findById(solicitud.params.id, function(error, cancion) {
+/**
+ * Consultar una Canción
+ */
+router.get('/:id', function (solicitud, respuesta) {
+  Cancion.findById(solicitud.params.id, function (error, cancion) {
     if (error) {
       console.error('Error consultando cancion por el ID: ', error)
+      respuesta.send('Error consultando la canción.')
     } else {
       respuesta.send({ cancion: cancion })
     }
   })
 })
 
-router.post('/', function(solicitud, respuesta) {
+/**
+ * Creación de una nueva canción
+ */
+router.post('/', function (solicitud, respuesta) {
   const nuevaCancion = new Cancion(solicitud.body)
-  nuevaCancion.save(function(error, cancionCreada) {
+  nuevaCancion.save(function (error, cancionCreada) {
     if (error) {
       console.error('Error creando canción: ', error)
+      respuesta.send('Error actualizando la canción.')
     } else {
       respuesta.send(cancionCreada)
     }
   })
+})
+
+/**
+ * Actualización de Canciones
+ */
+router.put('/:id', function(solicitud, respuesta) {
+  Cancion.findByIdAndUpdate(solicitud.params.id, solicitud.body, function(error, cancionVieja) {
+    if (error) {
+      console.error('Error actualizando la canción: ', error)
+      respuesta.send('Error actualizando la canción.')
+    } else {
+      Cancion.findById(solicitud.params.id, function (error, cancion) {
+        if (error) {
+          console.error('Error consultando la canción actualizada: ', error)
+          respuesta.send('Error consultando la canción actualizada.')
+        } else {
+          respuesta.send({ cancion: cancion })
+        }
+      })
+    }
+  })
+
 })
 
 module.exports = router
