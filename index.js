7dd1181898fe9908242393c62836dcc03887eccd
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-05-14 22:45:24 -0500

    Agregada libreria cors para permitir facilmente requests desde cualquier dominio.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const conectarBaseDeDatos = require('./bd')
 const rutasDeCanciones = require('./componentes/canciones/rutas')
 
@@ -10,6 +11,7 @@ const app = express()
 const PUERTO = 3000
 
 // Agregamos middlewares
+app.use(cors()) // Necesario para permitir requests desde cualquier dominio
 app.use(bodyParser.json())
 
 app.use('/canciones', rutasDeCanciones)
