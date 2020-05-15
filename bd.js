faaa1ad37481e53707d85a45cc2e376027d322f3
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-05-14 22:07:24 -0500

    Optimizada conexión a la base de datos.

--- bd.js
+++ bd.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose')
 
 // mongodb+srv://DB_USER:DB_PASSWORD@DB_HOST:DB_PORT/DB_NAME
-const conectarBaseDeDatos = function () {
-  mongoose.connect('mongodb://127.0.0.1:27017/bit_musica', {useNewUrlParser: true})
-
-  const db = mongoose.connection
-  db.on('error', console.error.bind(console, 'Error de conexión: '))
-  db.once('open', function() {
-    console.log('La conexión a la DB fue exitosa!')
+const conectarBaseDeDatos = async function () {
+  await mongoose.connect('mongodb://127.0.0.1:27017/bit_musica', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-
-  return db
+  console.log('La conexión a la DB fue exitosa!')
 }
 
 module.exports = conectarBaseDeDatos
