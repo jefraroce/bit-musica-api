bd5e30c25c4ed7dd26918e37d3b7b0f95ef350a0
yeison Herrera <yeison.herrera@gmail.com>
2020-05-21 21:38:35 -0500

    por fin la API

--- utilidades/funciones.js
+++ utilidades/funciones.js
@@ -1,10 +1,43 @@
-const responder = function (error, respuesta, valorAEnviarEnExito, mensajeDeError, codigoDeEstado) {
-  if (error) {
-    respuesta.status( codigoDeEstado || 500 ).json( { mensaje: mensajeDeError || error } )
-    console.error('[Error en Base De Datos] : ', error)
-  } else {
-    respuesta.status( codigoDeEstado || 200 ).json( valorAEnviarEnExito )
-  }
-}
+const responder = function(
+    error,
+    respuesta,
+    valorAEnviarEnExito,
+    mensajeDeError,
+    codigoDeEstado
+) {
+    if (error) {
+        respuesta
+            .status(codigoDeEstado || 500)
+            .json({ mensaje: mensajeDeError || error });
+        console.error("[Error en Base De Datos] : ", error);
+    } else {
+        respuesta.status(codigoDeEstado || 200).json(valorAEnviarEnExito);
+    }
+};
+const responderloggin = function(
+    error,
+    respuesta,
+    valorAEnviarEnExito,
+    mensajeDeError,
+    codigoDeEstado
+) {
+    if (error) {
+        respuesta
+            .status(codigoDeEstado || 500)
+            .json({ mensaje: mensajeDeError || error });
+        console.error("[Error en Base De Datos] : ", error);
+    } else if (valorAEnviarEnExito.length != 0) {
+        let respuestaAvatar = {
+            _id: valorAEnviarEnExito[0]._id,
+            nombre: valorAEnviarEnExito[0].nombre,
+            avatar: valorAEnviarEnExito[0].avatar,
+        };
+        respuesta.status(codigoDeEstado || 200).json(respuestaAvatar);
+    } else {
+        respuesta
+            .status(codigoDeEstado || 401)
+            .json({ mensaje: "Usuario no encontrado" });
+    }
+};
 
-module.exports = { responder }
+module.exports = { responder, responderloggin };
